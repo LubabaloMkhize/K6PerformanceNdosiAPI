8986b9da26bb46641fc52b78388bc1f59781800c
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-04-28 15:25:34 +0200

    Added update Testimonial Test.

---
+++ tests/updateTestimonialTest.js
@@ -0,0 +1,44 @@
+import { sleep } from 'k6';
+import { loginRequest } from '../requests/authRequest.js';
+import { createTestimonialRequest } from '../requests/createTestimonialRequest.js';
+import { updateTestimonialRequest } from '../requests/updateTestimonialRequest.js';
+import { validateUpdateTestimonialResponse } from '../validators/updateTestimonialValidator.js';
+import { TEST_CONFIG } from '../config/constants.js';
+import { PAYLOADS } from '../data/payloads.js';
+
+export const options = {
+    vus: TEST_CONFIG.vus,
+    duration: TEST_CONFIG.duration,
+    thresholds: {
+        http_req_failed: ['rate<0.01'],
+        checks: ['rate>0.95'],
+        http_req_duration: ['p(95)<1000'],
+    },
+};
+
+export default function updateTestimonialTest() {
+
+    
+    const loginResponse = loginRequest(PAYLOADS.login);
+    const token = loginResponse.json().data.token;
+
+    
+    const createResponse = createTestimonialRequest(
+        PAYLOADS.createTestimonial,
+        token
+    );
+
+    const testimonialId = createResponse.json().data.Id;
+
+   
+    const updateResponse = updateTestimonialRequest(
+        testimonialId,
+        PAYLOADS.updateTestimonial,
+        token
+    );
+
+    
+    validateUpdateTestimonialResponse(updateResponse);
+
+    sleep(TEST_CONFIG.sleepTime);
+}
