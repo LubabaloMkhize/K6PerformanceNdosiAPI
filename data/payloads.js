b7549fd693de162e6b80b7a2ab38693c2794c0d1
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-04-28 13:07:10 +0200

    Added a payload for the create testimonial.

--- data/payloads.js
+++ data/payloads.js
@@ -2,5 +2,11 @@ export const PAYLOADS = {
     login: {
         "email":"Tatalo.Mkhize@example.com",
         "password":"England@123456"
+    },
+    createTestimonial: {
+        "title": "From Beginner to Confident QA Professional",
+        "content": "The Ndosi Automation bootcamp gave me a solid foundation in software testing, both manual and automated. The hands-on projects and real-world scenarios helped me understand how QA works in an actual development environment. I now feel confident writing test cases, identifying defects, and collaborating with developers using Agile practices.",
+        "rating": 5,
+        "isPublic": true
     }
 };
