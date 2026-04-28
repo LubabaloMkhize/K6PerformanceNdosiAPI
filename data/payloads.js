d4a24fce706899da907cd0ce9b21aa8343c05f4a
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-04-28 15:06:56 +0200

    Added payload for updating Testimonial.

--- data/payloads.js
+++ data/payloads.js
@@ -8,5 +8,11 @@ export const PAYLOADS = {
         "content": "The Ndosi Automation bootcamp gave me a solid foundation in software testing, both manual and automated. The hands-on projects and real-world scenarios helped me understand how QA works in an actual development environment. I now feel confident writing test cases, identifying defects, and collaborating with developers using Agile practices.",
         "rating": 5,
         "isPublic": true
+    },
+    updateTestimonial: {
+        "title": "From Beginner to Confident QA Professional - Updated",
+        "content": "The Ndosi Automation bootcamp gave me a solid foundation in software testing, both manual and automated. The hands-on projects and real-world scenarios helped me understand how QA works in an actual development environment. I now feel confident writing test cases, identifying defects, and collaborating with developers using Agile practices.",
+        "rating": 4
     }
+
 };
