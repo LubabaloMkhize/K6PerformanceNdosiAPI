6450747b1db52bda35b4ce7b8e5e6d098830d81b
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-04-28 13:12:43 +0200

    Added create testimonial request.

---
+++ requests/createTestimonialRequest.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { URLS } from '../config/urls.js';
+
+export function createTestimonialRequest(payload, token){
+    const url = URLS.createTestimonial;
+    const body = JSON.stringify(payload);
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    };
+    return http.post(url, body, { headers });       
+}
