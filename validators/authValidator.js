b0becf33adad9d8f496d299607290a82d108a0eb
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-05-05 12:20:42 +0200

    Updated my auth validator.

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -1,27 +1,22 @@
 import { check } from 'k6';
 
+
 export function validateLoginResponse(response) {
     const body = response.json();
 
     const result = check(response, {
         'status is 200': (r) => r.status === 200,
-
-        'success is true': () => body.success === true,
-
-        'message is Login successful': () => 
-            body.message === 'Login successful',
-
-        'token exists': () => 
-            body.data && body.data.token !== undefined,
-
-        'user email is correct': () => 
-            body.data && body.data.user.email === 'Tatalo.Mkhize@example.com',
-
-        'response time < 2s': (r) => r.timings.duration < 5000,
+        'success is true': () => body?.success === true,
+        'message is Login successful': () =>
+            body?.message === 'Login successful',
+        'token exists': () => !!body?.data?.token,
+        'user email is correct': () =>
+            body?.data?.user?.email?.toLowerCase() ===
+            'tatalo.mkhize@example.com'.toLowerCase(),
     });
 
     if (!result) {
-        console.error('❌ Login validation failed');
+        console.error('❌ Login functional validation failed');
         console.error(`Status: ${response.status}`);
         console.error(`Body: ${response.body}`);
     }
