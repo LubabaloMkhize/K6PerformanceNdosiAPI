bf0e2e9b2caadc5c4fb51985c937b564ed6fbfe3
LubabaloMkhize <lubabalo.mkhize@gmail.com>
2026-05-05 22:42:21 +0200

    Updated yml file to include html report.

--- requests/createTestimonialRequest.js
+++ requests/createTestimonialRequest.js
@@ -1,14 +1,12 @@
 import http from 'k6/http';
 import { URLS } from '../config/urls.js';
+import {HEADERS} from '../config/constants.js';
 
 export function createTestimonialRequest(payload, token) {
     const url = URLS.testimonials; 
     const body = JSON.stringify(payload);
 
-    const headers = {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-    };
+    const headers = token ? {...HEADERS, Authorization: `Bearer ${token}`} : HEADERS;
 
     return http.post(url, body, { headers });
 }
