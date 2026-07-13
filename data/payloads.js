909d57cc500866138130ce99671a6af36143fba3
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-07-13 12:40:03 +0200

    updated payload.

--- data/payloads.js
+++ data/payloads.js
@@ -1,7 +1,7 @@
 export const PAYLOADS = {
     login: {
-        "email":"Tatalo@example.com",
-        "password":"@12345678"
+        "email":"Tatalo.Mkhize@example.com",
+        "password":"England@123456"
     },
     createTestimonial: {
         "title": "From Beginner to Confident QA Professional",
